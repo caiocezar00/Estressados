0d45e5f4caa9e6d181140c1c9f24818dec5a9d3a
caiocezar00 <111467290+caiocezar00@users.noreply.github.com>
2022-08-25 15:10:41 -0300

    Create filter.cpp

---
+++ filter.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int get_men(vector <int> vet){
+    vector<int> hom;
+    for(int elem : vet)
+        if(elem > 0)
+            hom.push_back(elem);
+        return hom;
+}
+int get_woman(vector <int> vet){
+    vector <int> mul;
+    for(int elem : vet)
+        if(elem >-10 && elem < 0)
+            mul.push_back(elem);
+        return mul;
+}
+int main()
